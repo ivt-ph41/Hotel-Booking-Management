7093a89a6323960e73d6bf87b00633cfbce7e53e
ngocphuocha <phuoc04012000@gmail.com>
2021-04-04 17:58:34 +0700

    fix bug

--- resources/views/bookings/user-booking.blade.php
+++ resources/views/bookings/user-booking.blade.php
@@ -1,21 +1,9 @@
 @extends('layouts.master')
 @section('title', 'My Booking')
 
-@section('css')
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
-  integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-@endsection
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-  integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
-</script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
-  integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
-</script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"
-  integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous">
-</script>
 
 @section('content')
+
 @if (session()->has('status'))
 <div class="alert alert-warning alert-dismissible fade show" role="alert">
   <button type="button" class="close" data-dismiss="alert" aria-label="Close">
@@ -57,31 +45,31 @@
         {{--Pending--}}
         @if ($booking->status == \App\Entities\Booking::PENDING_STATUS)
         <b class="text-warning mr-3">Pending</b>
-          {{-- if current day < date_start two day, user can Cancel booking --}}
-          @if ($flag = \Carbon\Carbon::now()->addDay(2)->toDateString() < $bookingDetail->date_start ? true : false)
-              <td>
-                <form class="form-inline" action="{{ route('bookings.cancel', $booking->id) }}" method="post">
-                  @csrf
-                  @method('DELETE')
-                  <div class="form-group">
-                    <button type="submit" class="btn btn-info">Cancel</button>
-                  </div>
-                </form>
-              </td>
-          @endif
+        {{-- if current day < date_start two day, user can Cancel booking --}}
+        @if ($flag = \Carbon\Carbon::now()->addDay(2)->toDateString() < $bookingDetail->date_start ? true : false)
+      <td>
+        <form class="form-inline" action="{{ route('bookings.cancel', $booking->id) }}" method="post">
+          @csrf
+          @method('DELETE')
+          <div class="form-group">
+            <button type="submit" class="btn btn-info">Cancel</button>
+          </div>
+        </form>
+      </td>
+      @endif
 
-        {{--Appprove--}}
-        @elseif ($booking->status == \App\Entities\Booking::APPROVE_STATUS)
-        <b class="text-success">Approve</b>
+      {{--Appprove--}}
+      @elseif ($booking->status == \App\Entities\Booking::APPROVE_STATUS)
+      <b class="text-success">Approve</b>
 
-        {{-- Cancel--}}
-        @elseif ($booking->status == \App\Entities\Booking::CANCEL_STATUS)
-        <b class="text-danger">Cancel</b>
+      {{-- Cancel--}}
+      @elseif ($booking->status == \App\Entities\Booking::CANCEL_STATUS)
+      <b class="text-danger">Cancel</b>
 
-        {{-- Finish --}}
-        @elseif ($booking->status == \App\Entities\Booking::FINISH_STATUS)
-        <b class="text-dark">{{__('Finish')}}</b>
-        @endif
+      {{-- Finish --}}
+      @elseif ($booking->status == \App\Entities\Booking::FINISH_STATUS)
+      <b class="text-dark">{{__('Finish')}}</b>
+      @endif
 
       </td>
     </tr>
