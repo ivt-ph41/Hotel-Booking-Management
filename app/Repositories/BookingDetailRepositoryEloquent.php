dd86ad92ae55ecff6951aac56e8da1b5413dd919
ngocphuocha <phuoc04012000@gmail.com>
2021-04-09 08:42:34 +0700

    fix show view manager booking

--- app/Repositories/BookingDetailRepositoryEloquent.php
+++ app/Repositories/BookingDetailRepositoryEloquent.php
@@ -7,6 +7,8 @@ use Prettus\Repository\Criteria\RequestCriteria;
 use App\Repositories\BookingDetailRepository;
 use App\Entities\BookingDetail;
 use App\Validators\BookingDetailValidator;
+use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Builder;
 
 /**
  * Class BookingDetailRepositoryEloquent.
@@ -15,23 +17,46 @@ use App\Validators\BookingDetailValidator;
  */
 class BookingDetailRepositoryEloquent extends BaseRepository implements BookingDetailRepository
 {
-    /**
-     * Specify Model class name
-     *
-     * @return string
-     */
-    public function model()
-    {
-        return BookingDetail::class;
+  /**
+   * Specify Model class name
+   *
+   * @return string
+   */
+  public function model()
+  {
+    return BookingDetail::class;
+  }
+
+
+
+  /**
+   * Boot up the repository, pushing criteria
+   */
+  public function boot()
+  {
+    $this->pushCriteria(app(RequestCriteria::class));
+  }
+
+  /**
+   * Return view manager booking status
+   *
+   * @param  mixed $request
+   * @return void
+   */
+  public function managerBooking(Request $request)
+  {
+    if ($request->has('search')) {
+      $data = $request->input('search');
+      // search booking by room name or user name of booking
+      $booking_details = $this->model->with(['booking', 'room'])->whereHas('booking', function (Builder $query) use ($data) {
+        $query->where('name', 'like', "%$data%");
+      })->orWhereHas('room', function (Builder $query) use ($data) {
+        $query->where('name', 'like', "%$data%");
+      })->get();
     }
+    // GET USER WITH BOOKING
+    $booking_details = $this->model->with(['room', 'booking'])->paginate(5);
 
-
-
-    /**
-     * Boot up the repository, pushing criteria
-     */
-    public function boot()
-    {
-        $this->pushCriteria(app(RequestCriteria::class));
-    }
+    return view('admins.manager-booking', compact('booking_details'));
+  }
 }
