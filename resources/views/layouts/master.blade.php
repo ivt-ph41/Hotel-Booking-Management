7e4ccef8acb035ee4b4fbbfd57858b168b7a376a
ngocphuocha <phuoc04012000@gmail.com>
2021-04-21 23:32:47 +0700

    fix UI layout

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -82,7 +82,7 @@
         <div class="row">
           <div class="col-lg-2">
             <div class="header__logo">
-              <a href="#"><img src="{{asset('hiroto-master/img/logo.png')}}" alt=""></a>
+              <a href="{{ route('/')}}"><img src="{{asset('hiroto-master/img/logo.png')}}" alt=""></a>
             </div>
           </div>
           <div class="col-lg-10">
