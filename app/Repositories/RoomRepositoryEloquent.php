dc0a9fe423860636cebef389ae78d6e2023f8429
ngocphuocha <phuoc04012000@gmail.com>
2021-04-25 23:16:09 +0700

    no change

--- app/Repositories/RoomRepositoryEloquent.php
+++ app/Repositories/RoomRepositoryEloquent.php
@@ -26,225 +26,225 @@ use App\Entities\Comment;
  */
 class RoomRepositoryEloquent extends BaseRepository implements RoomRepository
 {
-  /**
-   * Specify Model class name
-   *
-   * @return string
-   */
-  public function model()
-  {
-    return Room::class;
-  }
+    /**
+     * Specify Model class name
+     *
+     * @return string
+     */
+    public function model()
+    {
+        return Room::class;
+    }
 
 
-  /**
-   * Boot up the repository, pushing criteria
-   */
-  public function boot()
-  {
-    $this->pushCriteria(app(RequestCriteria::class));
-  }
+    /**
+     * Boot up the repository, pushing criteria
+     */
+    public function boot()
+    {
+        $this->pushCriteria(app(RequestCriteria::class));
+    }
 
-  /*
+    /*
   * Filter room by date and a number person/room
   */
-  public function filterRoom(Request $request)
-  {
-    //Make validator
-    $validator = Validator::make($request->all(), [
-      'date_start' => 'required|before:date_end',
-      'date_end' => 'required',
-      'person_room' => 'required'
-    ]);
-    // If validate fail then return redirect back with error
-    if ($validator->fails()) {
-      return redirect()->route('rooms.index')->with(['error' => 'Error date_start must be before date_end!']);
-    }
-    // Get list person_room_id to display for select option
-    $person_room_id = $request->input('person_room');
-
-    // convert date to format 'Y-m-d'
-    $date_start = $request->input('date_start');
-    $date_start = strtotime($date_start);
-    $date_start = date('Y-m-d', $date_start);
-
-    $date_end = $request->input('date_end');
-    $date_end = strtotime($date_end);
-    $date_end = date('Y-m-d', $date_end);
-
-    // Room have booking in booking detail
-    $roomNotAvailable = $this->model->whereHas('bookingDetails', function (Builder $query) use ($date_start, $date_end) {
-      $query->whereBetween('date_start', [$date_start, $date_end])
-        ->orWhereBetween('date_end', [$date_start, $date_end]);
-    })->pluck('id'); // get all id of room not available
-
-    // Room not have booking in booking detail
-    $roomAvailable = $this->model->with(['type', 'bed', 'bookingDetails'])
-      ->whereNotIn('id', $roomNotAvailable)
-      ->where('person_room_id', '=', $person_room_id)->paginate(3);
-
-    // Append to the query string of pagination links
-    $roomAvailable->appends([
-      'date_start' => $date_start,
-      'date_end' => $date_end,
-      'person_room' => $person_room_id
-    ]);
-    // Get list person/room in resource for 'filter room'
-    $person_room_list = PersonRoom::all();
-    return view('rooms.index', compact('roomAvailable', 'person_room_list'));
-  }
-
-  /**
-   * search room
-   */
-  public function searchRoom(Request $request)
-  {
-    // if request has search
-    if ($request->has('search')) {
-      // $query == value input search
-      $query = $request->input('search');
-      // Search room with room name
-      $rooms = $this->model->where('name', 'LIKE', "%$query%")->select('rooms.id', 'rooms.name')->get();
-
-      // return response with json with http status 200
-      return response()->json($rooms, 200);
-    }
-  }
-  /**
-   * store new room in resources
-   *
-   * @param  mixed $request
-   * @return void
-   */
-  public function storeRoom(CreateRoomRequest $request)
-  {
-    // start transaction
-    DB::beginTransaction();
-    try {
-      // create new room in rooms table
-      $room = $this->model->create($request->only('name', 'price', 'description', 'size', 'bed_id', 'type_id', 'person_room_id'));
-
-      // If request has file upload
-      if ($request->hasFile('images')) {
-        foreach ($request->file('images') as $key => $image) {
-          // Get file name inclue extention
-          $imageName = time() . $image->getClientOriginalname();
-          // Declare target dir contain image in public/images/rooms forder
-          $target_dir = 'images/rooms';
-          // Move images to target dir
-          $image->move($target_dir, $imageName);
-          // Add information to array for store new resource in images table
-          $array[$key]['path'] = $imageName;
-          $array[$key]['created_at'] = now();
-          $array[$key]['updated_at'] = now();
-          $array[$key]['room_id'] = $room->id;
+    public function filterRoom(Request $request)
+    {
+        //Make validator
+        $validator = Validator::make($request->all(), [
+            'date_start' => 'required|before:date_end',
+            'date_end' => 'required',
+            'person_room' => 'required'
+        ]);
+        // If validate fail then return redirect back with error
+        if ($validator->fails()) {
+            return redirect()->route('rooms.index')->with(['error' => 'Error date_start must be before date_end!']);
         }
-      }
-      // Insert new resource in images table with data = $array by using relationship
-      $room->images()->insert($array);
+        // Get list person_room_id to display for select option
+        $person_room_id = $request->input('person_room');
 
-      // All OK then commit
-      DB::commit();
-    } catch (\Exception $e) {
-      // something went wrong
-      DB::rollback();
-      // return redirect back with session error
-      return redirect()->back()->with(['error' => 'Something went wrong, please try again!']);
+        // convert date to format 'Y-m-d'
+        $date_start = $request->input('date_start');
+        $date_start = strtotime($date_start);
+        $date_start = date('Y-m-d', $date_start);
+
+        $date_end = $request->input('date_end');
+        $date_end = strtotime($date_end);
+        $date_end = date('Y-m-d', $date_end);
+
+        // Room have booking in booking detail
+        $roomNotAvailable = $this->model->whereHas('bookingDetails', function (Builder $query) use ($date_start, $date_end) {
+            $query->whereBetween('date_start', [$date_start, $date_end])
+                ->orWhereBetween('date_end', [$date_start, $date_end]);
+        })->pluck('id'); // get all id of room not available
+
+        // Room not have booking in booking detail
+        $roomAvailable = $this->model->with(['type', 'bed', 'bookingDetails'])
+            ->whereNotIn('id', $roomNotAvailable)
+            ->where('person_room_id', '=', $person_room_id)->paginate(3);
+
+        // Append to the query string of pagination links
+        $roomAvailable->appends([
+            'date_start' => $date_start,
+            'date_end' => $date_end,
+            'person_room' => $person_room_id
+        ]);
+        // Get list person/room in resource for 'filter room'
+        $person_room_list = PersonRoom::all();
+        return view('rooms.index', compact('roomAvailable', 'person_room_list'));
     }
-    // return route manager room with session 'create success'
-    return redirect()->route('admins.room.manager')->with(['create success' => 'Success']);
-  }
 
-  /**
-   * Return view table manager room
-   */
-  public function showViewManagerRoom(Request $request)
-  {
-    // If have search action from user
-    if ($request->has('search')) {
-      $rooms = $this->model->where('name', 'LIKE', '%' . $request->input('search') . '%')->paginate(5);
+    /**
+     * search room
+     */
+    public function searchRoom(Request $request)
+    {
+        // if request has search
+        if ($request->has('search')) {
+            // $query == value input search
+            $query = $request->input('search');
+            // Search room with room name
+            $rooms = $this->model->where('name', 'LIKE', "%$query%")->select('rooms.id', 'rooms.name')->get();
 
-      // Append to the query string of pagination links
-      $rooms->appends(['search' => $request->input('search')]);
-
-      // if not have result
-      if (count($rooms) == 0) {
-        $noResultFound = '';
-        // dd($bookings->toArray());
-        return view('admins.manager-rooms', compact('rooms', 'noResultFound'));
-      }
-      // get total of result
-      $totalResult = $rooms->total();
-
-      // return $bookings with search query and display total result
-      return view('admins.manager-rooms', compact('rooms', 'totalResult'));
+            // return response with json with http status 200
+            return response()->json($rooms, 200);
+        }
     }
-    // Default: get all room
-    $rooms = $this->model->orderBy('id', 'desc')->paginate(5);
+    /**
+     * store new room in resources
+     *
+     * @param  mixed $request
+     * @return void
+     */
+    public function storeRoom(CreateRoomRequest $request)
+    {
+        // start transaction
+        DB::beginTransaction();
+        try {
+            // create new room in rooms table
+            $room = $this->model->create($request->only('name', 'price', 'description', 'size', 'bed_id', 'type_id', 'person_room_id'));
 
-    return view('admins.manager-rooms', compact('rooms'));
-  }
+            // If request has file upload
+            if ($request->hasFile('images')) {
+                foreach ($request->file('images') as $key => $image) {
+                    // Get file name inclue extention
+                    $imageName = time() . $image->getClientOriginalname();
+                    // Declare target dir contain image in public/images/rooms forder
+                    $target_dir = 'images/rooms';
+                    // Move images to target dir
+                    $image->move($target_dir, $imageName);
+                    // Add information to array for store new resource in images table
+                    $array[$key]['path'] = $imageName;
+                    $array[$key]['created_at'] = now();
+                    $array[$key]['updated_at'] = now();
+                    $array[$key]['room_id'] = $room->id;
+                }
+            }
+            // Insert new resource in images table with data = $array by using relationship
+            $room->images()->insert($array);
 
-  /**
-   * Update room
-   */
-  public function updateRoom($id, EditRoomRequest $request)
-  {
-    // Update room where room id = $id
-    $this->model->where('id', $id)->update($request->except('images', '_token', '_method'));
-    // if success then return view manager room with session success
-    return redirect()->route('admins.room.manager')->with(['update success' => 'Update success']);
-  }
+            // All OK then commit
+            DB::commit();
+        } catch (\Exception $e) {
+            // something went wrong
+            DB::rollback();
+            // return redirect back with session error
+            return redirect()->back()->with(['error' => 'Something went wrong, please try again!']);
+        }
+        // return route manager room with session 'create success'
+        return redirect()->route('admins.room.manager')->with(['create success' => 'Success']);
+    }
 
-  /**
-   * destroy Room if room not have booking with status approve and pending
-   *
-   * @param  mixed $id (room id)
-   * @return void
-   */
-  public function destroyRoom($id)
-  {
-    /* Retrieve current booking
+    /**
+     * Return view table manager room
+     */
+    public function showViewManagerRoom(Request $request)
+    {
+        // If have search action from user
+        if ($request->has('search')) {
+            $rooms = $this->model->where('name', 'LIKE', '%' . $request->input('search') . '%')->paginate(5);
+
+            // Append to the query string of pagination links
+            $rooms->appends(['search' => $request->input('search')]);
+
+            // if not have result
+            if (count($rooms) == 0) {
+                $noResultFound = '';
+                // dd($bookings->toArray());
+                return view('admins.manager-rooms', compact('rooms', 'noResultFound'));
+            }
+            // get total of result
+            $totalResult = $rooms->total();
+
+            // return $bookings with search query and display total result
+            return view('admins.manager-rooms', compact('rooms', 'totalResult'));
+        }
+        // Default: get all room
+        $rooms = $this->model->orderBy('id', 'desc')->paginate(5);
+
+        return view('admins.manager-rooms', compact('rooms'));
+    }
+
+    /**
+     * Update room
+     */
+    public function updateRoom($id, EditRoomRequest $request)
+    {
+        // Update room where room id = $id
+        $this->model->where('id', $id)->update($request->except('images', '_token', '_method'));
+        // if success then return view manager room with session success
+        return redirect()->route('admins.room.manager')->with(['update success' => 'Update success']);
+    }
+
+    /**
+     * destroy Room if room not have booking with status approve and pending
+     *
+     * @param  mixed $id (room id)
+     * @return void
+     */
+    public function destroyRoom($id)
+    {
+        /* Retrieve current booking
     has status approve or pending with room id */
-    $booking_details = BookingDetail::whereHas('booking', function (Builder $query) {
-      $query->whereIn('status', [Booking::APPROVE_STATUS, Booking::PENDING_STATUS]);
-    })->where('room_id', $id)->get();
-    // dd($booking_details);
-    /* If $booking_detail == [] (not have booking with approve or pending)
+        $booking_details = BookingDetail::whereHas('booking', function (Builder $query) {
+            $query->whereIn('status', [Booking::APPROVE_STATUS, Booking::PENDING_STATUS]);
+        })->where('room_id', $id)->get();
+        // dd($booking_details);
+        /* If $booking_detail == [] (not have booking with approve or pending)
     then can delete this room */
-    if (count($booking_details) == 0) {
-      DB::beginTransaction();
+        if (count($booking_details) == 0) {
+            DB::beginTransaction();
 
-      try {
-        $bookings = Booking::with(['bookingDetails' => function ($query) use ($id) {
-          return $query->where('room_id', $id);
-        }])->get();
-        // dd($bookings->toArray());
-        foreach ($bookings as $booking) {
-          foreach ($booking->bookingDetails as $bookingDetail) {
-            // Delete booking detail
-            BookingDetail::destroy($bookingDetail->id);
-          }
-          // Delete booking
-          Booking::destroy($booking->id);
+            try {
+                $bookings = Booking::with(['bookingDetails' => function ($query) use ($id) {
+                    return $query->where('room_id', $id);
+                }])->get();
+                // dd($bookings->toArray());
+                foreach ($bookings as $booking) {
+                    foreach ($booking->bookingDetails as $bookingDetail) {
+                        // Delete booking detail
+                        BookingDetail::destroy($bookingDetail->id);
+                    }
+                    // Delete booking
+                    Booking::destroy($booking->id);
+                }
+                // Delete images of room
+                Image::where('room_id', $id)->delete();
+                // Delete comment of room
+                Comment::where('room_id', $id)->delete();
+                // Delete room
+                $this->model->where('id', $id)->delete();
+
+                // all good
+                DB::commit();
+            } catch (\Exception $e) {
+                DB::rollback();
+                // something went wrong
+                return redirect()->back()->with(['something wrong' => 'Something went wrong, please try again!']);
+            }
+            return redirect()->route('admins.room.manager')->with(['delete success' => 'Delete Success!']);
         }
-        // Delete images of room
-        Image::where('room_id', $id)->delete();
-        // Delete comment of room
-        Comment::where('room_id', $id)->delete();
-        // Delete room
-        $this->model->where('id', $id)->delete();
-
-        // all good
-        DB::commit();
-      } catch (\Exception $e) {
-        DB::rollback();
-        // something went wrong
-        return redirect()->back()->with(['something wrong' => 'Something went wrong, please try again!']);
-      }
-      return redirect()->route('admins.room.manager')->with(['delete success' => 'Delete Success!']);
+        // default
+        return redirect()->back()->with(['delete fail' => 'Delete fail!']);
     }
-    // default
-    return redirect()->back()->with(['delete fail' => 'Delete fail!']);
-  }
 }
--- app/Repositories/UserRepositoryEloquent.php
+++ app/Repositories/UserRepositoryEloquent.php
@@ -26,155 +26,155 @@ use App\Entities\Profile;
  */
 class UserRepositoryEloquent extends BaseRepository implements UserRepository
 {
-  /**
-   * Specify Model class name
-   *
-   * @return string
-   */
-  public function model()
-  {
-    return User::class;
-  }
-
-
-
-  /**
-   * Boot up the repository, pushing criteria
-   */
-  public function boot()
-  {
-    $this->pushCriteria(app(RequestCriteria::class));
-  }
-
-  /**
-   * Register user
-   */
-  public function register(RegisterUserRequest $request)
-  {
-    $data = $request->only('email', 'password');
-    $data['password'] = Hash::make($data['password']);
-
-    // Get User's role = 2
-    $data['role_id'] = \App\Entities\Role::USER_ROLE;
-
-
-    DB::beginTransaction();
-    try {
-      // Begin a transaction
-
-      // Do something and save to the db...
-
-      // Store new resource to users table
-      $user = $this->model->create($data);
-      // Store new resource to profiles table with $user
-      $user->profile()->create($request->only('name', 'phone', 'address'));
-
-      // Commit the transaction
-      DB::commit();
-    } catch (\Exception $e) {
-      // An error occured; cancel the transaction...
-      DB::rollback();
-      // and throw the error again.
-      throw new \Exception($e->getMessage());
+    /**
+     * Specify Model class name
+     *
+     * @return string
+     */
+    public function model()
+    {
+        return User::class;
     }
 
-    return $user;
-  }
 
-  public function updatePassword(ChangePasswordRequest $request)
-  {
-    if (Hash::check($request->input('current_password'), Auth::user()->password)) {
-      // Update new password
-      $this->model->where('id', Auth::id())->update([
-        'password' => Hash::make($request->input('new_password'))
-      ]);
-      return redirect()->route('users.profile')->with(['password-success' => 'Your current password is update']);
+
+    /**
+     * Boot up the repository, pushing criteria
+     */
+    public function boot()
+    {
+        $this->pushCriteria(app(RequestCriteria::class));
     }
-    return redirect()->back()->withInput()->with(['error' => 'Your current password is not correct!']);
-  }
 
-  public function showViewManagerUser(Request $request)
-  {
-    // If have search action from user
-    if ($request->has('search')) {
-      $data = $request->input('search');
-      // search user with profile using relationship
-      $users = $this->model->with(['profile'])
-                    ->where('role_id', Role::USER_ROLE)
-                    ->where('email', 'LIKE', "%$data%")
-                    ->paginate(3);
-      $users->appends(['search' => $request->input('search')]);
+    /**
+     * Register user
+     */
+    public function register(RegisterUserRequest $request)
+    {
+        $data = $request->only('email', 'password');
+        $data['password'] = Hash::make($data['password']);
 
-      // if not have result
-      if (count($users) == 0) {
-        $noResultFound = '';
-        // dd($bookings->toArray());
-        return view('admins.users.manager-users', compact('users', 'noResultFound'));
-      }
-      // get total of result
-      $totalResult = $users->total();
+        // Get User's role = 2
+        $data['role_id'] = \App\Entities\Role::USER_ROLE;
 
-      // return $bookings with search query and display total result
-      return view('admins.users.manager-users', compact('users', 'totalResult'));
+
+        DB::beginTransaction();
+        try {
+            // Begin a transaction
+
+            // Do something and save to the db...
+
+            // Store new resource to users table
+            $user = $this->model->create($data);
+            // Store new resource to profiles table with $user
+            $user->profile()->create($request->only('name', 'phone', 'address'));
+
+            // Commit the transaction
+            DB::commit();
+        } catch (\Exception $e) {
+            // An error occured; cancel the transaction...
+            DB::rollback();
+            // and throw the error again.
+            throw new \Exception($e->getMessage());
+        }
+
+        return $user;
     }
-    // Default: get all user paginate
-    // $users = $this->model->with(['profile'])->select('users.*', 'profiles.*')->orderBy('profiles.name')->paginate(5);
-    $users = $this->model->with(['profile'])->where('role_id', Role::USER_ROLE)->orderBy('email')->paginate(3);
-    // dd($users->toArray());
-    return view('admins.users.manager-users', compact('users'));
-  }
 
-  /**
-   * Delete user
-   */
-  public function deleteUser($id)
-  {
-    // get booking with approve and pending status
-    $booking = Booking::where('user_id', $id)
-      ->whereIn('status', [Booking::APPROVE_STATUS, Booking::PENDING_STATUS])
-      ->get();
+    public function updatePassword(ChangePasswordRequest $request)
+    {
+        if (Hash::check($request->input('current_password'), Auth::user()->password)) {
+            // Update new password
+            $this->model->where('id', Auth::id())->update([
+                'password' => Hash::make($request->input('new_password'))
+            ]);
+            return redirect()->route('users.profile')->with(['password-success' => 'Your current password is update']);
+        }
+        return redirect()->back()->withInput()->with(['error' => 'Your current password is not correct!']);
+    }
 
-    // IF USER HAVE BOOKING STATUS not have PENDING OR APPROVE can delete
-    if ($booking->count() == 0) {
-      DB::beginTransaction();
+    public function showViewManagerUser(Request $request)
+    {
+        // If have search action from user
+        if ($request->has('search')) {
+            $data = $request->input('search');
+            // search user with profile using relationship
+            $users = $this->model->with(['profile'])
+                ->where('role_id', Role::USER_ROLE)
+                ->where('email', 'LIKE', "%$data%")
+                ->paginate(3);
+            $users->appends(['search' => $request->input('search')]);
 
-      try {
-        // get current booking of user
-        $user = $this->model->with(['bookings'])->find($id);
-
-        if ($user->bookings->count() != 0) { // if user have booking then delete booking
-          // Get all bookings of user include booking detail
-          $user_bookings = Booking::with('bookingDetails')->where('user_id', $id)->get();
-
-          // Delete booking detail with booking id first then delete booking with booking id
-          foreach ($user_bookings as $val) {
-            if ($val->bookingDetails->count() != 0) {   //if bookingDetail not null(for sure)
-              BookingDetail::where('booking_id', $val->id)->delete();
-              Booking::destroy($id);
-            } else {
-              //only delete booking with booking id
-              Booking::destroy($id);
+            // if not have result
+            if (count($users) == 0) {
+                $noResultFound = '';
+                // dd($bookings->toArray());
+                return view('admins.users.manager-users', compact('users', 'noResultFound'));
             }
-          }
-        }
-        // Delete comment of user
-        Comment::where('user_id', $id)->delete();
-        //Delete profile  with user_id = $id
-        $profile = Profile::where('user_id', $id)->delete();
-        if ($profile) { // if delete profile success
-          // Then delete user with $id
-          $this->model->destroy($id);
-        }
+            // get total of result
+            $totalResult = $users->total();
 
-        //commit
-        DB::commit();
-        return redirect()->route('admins.user.manager')->with(['delete success' => 'Delete success!']);
-      } catch (\Exception $e) {
-        DB::rollBack();
-        return redirect()->back()->with(['something error' => 'Something went wrong, please try again!']);
-      }
-    } else {
-      return redirect()->back()->with(['delete fail' => 'Fail! This current user have booking with status pending or approve.)']);
+            // return $bookings with search query and display total result
+            return view('admins.users.manager-users', compact('users', 'totalResult'));
+        }
+        // Default: get all user paginate
+        // $users = $this->model->with(['profile'])->select('users.*', 'profiles.*')->orderBy('profiles.name')->paginate(5);
+        $users = $this->model->with(['profile'])->where('role_id', Role::USER_ROLE)->orderBy('email')->paginate(3);
+        // dd($users->toArray());
+        return view('admins.users.manager-users', compact('users'));
     }
-  }
+
+    /**
+     * Delete user
+     */
+    public function deleteUser($id)
+    {
+        // get booking with approve and pending status
+        $booking = Booking::where('user_id', $id)
+            ->whereIn('status', [Booking::APPROVE_STATUS, Booking::PENDING_STATUS])
+            ->get();
+
+        // IF USER HAVE BOOKING STATUS not have PENDING OR APPROVE can delete
+        if ($booking->count() == 0) {
+            DB::beginTransaction();
+
+            try {
+                // get current booking of user
+                $user = $this->model->with(['bookings'])->find($id);
+
+                if ($user->bookings->count() != 0) { // if user have booking then delete booking
+                    // Get all bookings of user include booking detail
+                    $user_bookings = Booking::with('bookingDetails')->where('user_id', $id)->get();
+
+                    // Delete booking detail with booking id first then delete booking with booking id
+                    foreach ($user_bookings as $val) {
+                        if ($val->bookingDetails->count() != 0) {   //if bookingDetail not null(for sure)
+                            BookingDetail::where('booking_id', $val->id)->delete();
+                            Booking::destroy($id);
+                        } else {
+                            //only delete booking with booking id
+                            Booking::destroy($id);
+                        }
+                    }
+                }
+                // Delete comment of user
+                Comment::where('user_id', $id)->delete();
+                //Delete profile  with user_id = $id
+                $profile = Profile::where('user_id', $id)->delete();
+                if ($profile) { // if delete profile success
+                    // Then delete user with $id
+                    $this->model->destroy($id);
+                }
+
+                //commit
+                DB::commit();
+                return redirect()->route('admins.user.manager')->with(['delete success' => 'Delete success!']);
+            } catch (\Exception $e) {
+                DB::rollBack();
+                return redirect()->back()->with(['something error' => 'Something went wrong, please try again!']);
+            }
+        } else {
+            return redirect()->back()->with(['delete fail' => 'Fail! This current user have booking with status pending or approve.)']);
+        }
+    }
 }
